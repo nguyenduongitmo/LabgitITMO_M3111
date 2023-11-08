384d8f1d6e5132224b80964f868ef9925a942de7
nguyenduongitmo <142491870+nguyenduongitmo@users.noreply.github.com>
2023-11-08 16:07:06 +0700

    Add unittest for function of rectangle

--- rectangle.py
+++ rectangle.py
@@ -1,7 +1,24 @@
-def area(a, b): 
-    '''Принимает число a и b (длина и ширина прямоугольника), возвращает площадь  прямоугольника'''
+import unittest
+
+
+def area(a, b):
+    """Принимает число a и b (длина и ширина прямоугольника), возвращает площадь  прямоугольника"""
     return a * b 
-    
+
+
 def perimeter(a, b): 
-    '''Принимает число a и b (длина и ширина прямоугольника), возвращает периметр прямоугольника'''
+    """Принимает число a и b (длина и ширина прямоугольника), возвращает периметр прямоугольника"""
     return (a + b)*2
+
+
+class RectangleTestCase(unittest.TestCase):
+    def test_rectangle1(self):
+        result = area(2, 3)
+        self.assertEqual(result, 6)
+
+    def test_retangle2(self):
+        result = perimeter(4, 8)
+        self.assertEqual(result,  24)
+
+if __name__ == '__main__':
+    unittest.main()
